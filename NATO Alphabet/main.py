50958c10f86bd0153ecbea274adc4c91dccde290
nsen2000 <157868613+nsen2000@users.noreply.github.com>
2024-02-06 20:28:44 +1100

    NATO Alphabet Project

---
+++ NATO Alphabet/main.py
@@ -0,0 +1,31 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+{"A": "Alfa", "B": "Bravo"}
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
+# print(alphabet)
+
+word = input("Please input a word: ").upper()
+code = [alphabet[letter] for letter in word]
+print(code)
